9208eb43079dabad09ac92034f7ac9828f578fe5
Paper Huang <shan@arcblock.io>
2019-11-20 11:17:14 +0800

    fix ci

--- examples/src/test/java/io/arcblock/forge/did/DidAuthUtilsTest.kt
+++ examples/src/test/java/io/arcblock/forge/did/DidAuthUtilsTest.kt
@@ -1,8 +1,6 @@
 package io.arcblock.forge.did
 
 import io.arcblock.forge.did.bean.AppInfo
-import io.arcblock.forge.did.bean.MetaInfo
-import io.arcblock.forge.did.bean.ProfileClaim
 import org.junit.Test
 
 class DidAuthUtilsTest {
@@ -11,7 +9,7 @@ class DidAuthUtilsTest {
     // declare your application app info
     val appInfo = AppInfo()
     // declare a claim
-    val claim1 = ProfileClaim(MetaInfo("profile info", ""), arrayListOf("username", "email"))
+    //val claim1 = ProfileClaim(MetaInfo("profile info", ""), arrayListOf("username", "email"))
 
     // DidAuthUtils.createDidAuthToken(arrayOf(claim1),appInfo,System.currentTimeMillis(),wallet)
   }
